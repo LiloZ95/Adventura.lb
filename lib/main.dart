776483cb1c599998db9859c0484da14691786f66
Youhaoss <youhaossman211@gmail.com>
2025-04-11 22:56:34 +0300

    Add Firebase configuration and initialization
    - Create google-services.json for Firebase setup
    - Update settings.gradle to include Google services plugin
    - Add firebase.json for FlutterFire configuration
    - Implement firebase_options.dart for platform-specific Firebase options
    - Modify main.dart to initialize Firebase with options
    - Update pubspec.yaml to include firebase_core dependency

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,15 @@ import 'package:adventura/main_api.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
-  
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   try {
     await dotenv.load(fileName: ".env");
     print("✅ .env file loaded successfully");
