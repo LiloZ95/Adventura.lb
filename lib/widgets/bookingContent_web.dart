e752c6c30b7bda9ab185523841fcc32ff5fcd632
Reem <rmerheb@telepaty.org>
2025-04-13 13:30:08 +0300

    Updated bookingContent_web with latest changes

--- lib/widgets/bookingContent_web.dart
+++ lib/widgets/bookingContent_web.dart
@@ -227,6 +227,7 @@ class BookingContentWidget extends StatelessWidget {
                                 ),
                               ),
                             ],
+
                           ),
                         ),
                         Container(
