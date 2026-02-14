16f0a3b958663517295e646677ed8f3ca4a30cbe
Khalil Kurdi <129593490+LiloZ95@users.noreply.github.com>
2026-02-14 11:47:24 +0200

    Comment out Mapbox access token in config.dart
    Comment out the Mapbox access token for security.

--- lib/config.dart
+++ lib/config.dart
@@ -10,7 +10,7 @@ const String wsUrl =
     'ws://localhost:4000'; // For physical device it should be your local IP address instead of localhost
 
 // const String MAPBOX_ACCESS_TOKEN =
-//     'pk.eyJ1IjoiYWR2ZW50dXJhLWxiIiwiYSI6ImNtOGx4aHY0ZjE3c3UyanNkbnNrZW53bWwifQ.KlXTdlwtJmOJHnW29SLaAA'; // Your Mapbox access token
+//     ''; // Your Mapbox access token
 
 const String chabotUrl = "https://6db5-34-169-159-132.ngrok-free.app";
 
